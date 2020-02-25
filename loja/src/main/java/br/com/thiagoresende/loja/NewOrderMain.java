3e46e5a2e3bcc6da9e8acf983eb3ac0e0b8e0566
Thiago Resende <thiago.gcresende@gmail.com>
2020-02-25 12:27:07 -0300

    consumidores

--- loja/src/main/java/br/com/thiagoresende/loja/NewOrderMain.java
+++ loja/src/main/java/br/com/thiagoresende/loja/NewOrderMain.java
@@ -1,10 +1,31 @@
 package br.com.thiagoresende.loja;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
-    public static void main(String[] args) {
-        KafkaProducer<>
-        System.out.println("teste");
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties());
+        ProducerRecord<String, String> record = new ProducerRecord<String, String>("NEW_ORDERS", "123", "abc");
+        producer.send(record, (data, ex) -> {
+            if (ex != null) {
+                ex.printStackTrace();
+                return;
+            }
+            System.out.println(data.topic() + ":::partition" + data.partition()  + "/offset" + data.offset()  + "/timestamp" + data.timestamp());
+        }).get();
+    }
+
+    private static Properties properties() {
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        return properties;
     }
 }
