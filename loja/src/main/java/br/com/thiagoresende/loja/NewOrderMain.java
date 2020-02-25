91a77022ee574659698593eda778af1fc23ce2a6
Thiago Resende <thiago.gcresende@gmail.com>
2020-02-25 12:03:44 -0300

    Projeto Inicial

---
+++ loja/src/main/java/br/com/thiagoresende/loja/NewOrderMain.java
@@ -0,0 +1,10 @@
+package br.com.thiagoresende.loja;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+
+public class NewOrderMain {
+    public static void main(String[] args) {
+        KafkaProducer<>
+        System.out.println("teste");
+    }
+}
